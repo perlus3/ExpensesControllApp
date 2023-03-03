c7eff3d5318105b06edab99eafd8b99163597cda
Kamil <perlegus@gmail.com>
2023-03-03 14:13:16 +0100

    config for auth by jwt, user entity updated, dto added, and auth interfaces

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import {TypeormImports} from "./typeorm";
-import {UsersController} from "./controllers/users/users.controller";
-import {UsersService} from "./services/users/users.service";
+import { TypeormImports } from './typeorm';
+import { UsersController } from './controllers/users/users.controller';
+import { UsersService } from './services/users/users.service';
+import { AccountsController } from './controllers/accounts/accounts.controller';
+import { AccountsService } from './services/accounts/accounts.service';
+import { AuthController } from './controllers/auth/auth.controller';
+import { AuthService } from './services/auth/auth.service';
+import { JwtStrategy } from './helpers/auth/jwt.strategy';
+import JwtAuthGuard from './helpers/auth/jwt-auth.guard';
+import JwtRefreshGuard from './helpers/auth/jwt.refreshGuard';
+import { JwtRefreshTokenStrategy } from './helpers/auth/jwt.refreshToken.strategy';
+
 @Module({
   imports: [...TypeormImports],
-  controllers: [UsersController,],
-  providers: [UsersService],
+  controllers: [UsersController, AccountsController, AuthController],
+  providers: [
+    UsersService,
+    AccountsService,
+    AuthService,
+    JwtStrategy,
+    JwtRefreshTokenStrategy,
+    JwtRefreshGuard,
+    JwtAuthGuard.GlobalProtected(),
+  ],
 })
 export class AppModule {}
--- src/controllers/users/users.controller.ts
+++ src/controllers/users/users.controller.ts
@@ -1,21 +1,4 @@
-import { Controller, Get, Post, Req, Body } from '@nestjs/common';
-import {UsersEntity} from "../../entities/users.entity";
-import {RegisterUserDto} from "../../dtos/registerUser.dto";
-import {UsersService} from "../../services/users/users.service";
+import { Controller } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {
-    constructor(private usersService: UsersService,) {}
-
-    // @Get('me')
-    // async getMyProfile(@Req() req: Request): Promise<UsersEntity> {
-    //     console.log(req)
-    //     return req.user as UsersEntity;
-    // }
-
-    @Post('/register')
-    async register(@Body() registerData: RegisterUserDto) {
-        const user = await this.usersService.register(registerData);
-        return user;
-    }
-}
+export class UsersController {}
