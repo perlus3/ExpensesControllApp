0f63f9443f5e40b71c1026a91477b7d47b7a76dc
Kamil <perlegus@gmail.com>
2023-03-03 14:16:55 +0100

    bcrypt methods updated, JWTModule config, refresh-token entity added

--- src/helpers/password.ts
+++ src/helpers/password.ts
@@ -1,11 +1,14 @@
 import { compare, genSalt, hash } from 'bcrypt';
 
-export async function hashPassword(password: string): Promise<string> {
+export async function hashMethod(password: string): Promise<string> {
   const salt = await genSalt();
 
   return hash(password, salt);
 }
 
-export async function comparePassword(password: string, hashPassword: string): Promise<boolean> {
+export async function compareMethod(
+  password: string,
+  hashPassword: string,
+): Promise<boolean> {
   return compare(password, hashPassword);
 }
--- src/typeorm.ts
+++ src/typeorm.ts
@@ -1,10 +1,20 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from './configs/typeorm';
 import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { config } from './configs/config';
 
 const rootModule = TypeOrmModule.forRoot(dataSourceOptions);
 
 export const TypeormImports = [
-    rootModule,
-    TypeOrmModule.forFeature(dataSourceOptions.entities as EntityClassOrSchema[]),
+  rootModule,
+  JwtModule.register({
+    secret: config.JWT_SECRET,
+    signOptions: { expiresIn: config.JWT_EXPIRES_ACCESS },
+  }),
+  PassportModule.register({
+    session: false,
+  }),
+  TypeOrmModule.forFeature(dataSourceOptions.entities as EntityClassOrSchema[]),
 ];
