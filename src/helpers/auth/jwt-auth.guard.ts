0ec33adfef2b8c37e2af661d35b4e7cced03ba67
Kamil <perlegus@gmail.com>
2023-04-12 00:07:31 +0200

    zmiana typów w canActivate ( poprawa błędu podczas produkcji)

--- src/helpers/auth/jwt-auth.guard.ts
+++ src/helpers/auth/jwt-auth.guard.ts
@@ -1,6 +1,7 @@
 import { ExecutionContext, Injectable, SetMetadata } from '@nestjs/common';
 import { APP_GUARD, Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
 export const IS_PUBLIC_KEY = 'AuthGuard_Public';
 
@@ -19,7 +20,9 @@ export default class JwtAuthGuard extends AuthGuard('jwt') {
     super();
   }
 
-  canActivate(context: ExecutionContext) {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
     const isPublicRequest = this.reflector.getAllAndOverride<boolean>(
       IS_PUBLIC_KEY,
       [context.getHandler(), context.getClass()],
@@ -30,15 +33,4 @@ export default class JwtAuthGuard extends AuthGuard('jwt') {
 
     return super.canActivate(context);
   }
-
-  // private isPublicRequest(context: ExecutionContext): boolean {
-  //   return this.reflector.getAllAndOverride<boolean>(
-  //     IS_PUBLIC_KEY,
-  //     this.getTargets(context),
-  //   );
-  // }
-
-  // private getTargets(context: ExecutionContext) {
-  //   return [context.getHandler(), context.getClass()];
-  // }
 }
